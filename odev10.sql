a3032740c74c43708bff1887a0a58119eb813ff3
Onurcan Sevinc <onurcan.svnc@yandex.com>
2025-05-04 09:24:54 +0300

    add: odev10

---
+++ odev10.sql
@@ -0,0 +1,26 @@
+/*
+Ödev 10
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+Kolay Gelsin.
+*/
+
+/*
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+*/
+SELECT city, country FROM city LEFT JOIN country ON country.country_id = city.country_id;
+
+/*
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+*/
+SELECT payment_id, first_name, last_name FROM payment RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
+
+/*
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+*/
+SELECT rental_id, first_name, last_name FROM rental FULL JOIN customer ON rental.customer_id = customer.customer_id;
